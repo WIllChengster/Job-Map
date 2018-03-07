3f128c6992d6a237124b0688557a93332ae48cb1
William Cheng <willchengsters@gmail.com>
2018-03-07 13:26:29 -0800

    merge conflict in main.js

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
 $(document).ready(initializeApp);
-
 function initializeApp(){
     $('#jSearch').click(newSearch);
-    $('#jSearch').click(function(){
-        $('.landing').css('display', 'none');
-    })
-    $('.inner').click(function(){
-        $('.item1').toggleClass('spinItem1');
-        $('.item2').toggleClass('spinItem2');
-        $('.item3').toggleClass('spinItem3');
-        $('.item4').toggleClass('spinItem4');
-        $('.outer').toggleClass('hideBorder')
-    })
+    attachEventHandlers();
     getJobData();
 }
 
@@ -19,6 +9,7 @@ function newSearch(){
     let title = $('#jTitle').val();
     let location = $('#jLocal').val();
     var findJobs = new startSearch(title, location);
+    console.log(findJobs);
 }
 
 class startSearch{
--- pageAnim.js
+++ pageAnim.js
@@ -1,14 +1,10 @@
-$(document).ready(initializeApp);
-
-function initializeApp(){
-    attachEventHandlers()
-}
-
 function attachEventHandlers(){
     $('#jSearch').click(function(){
         $('.landing').css('display', 'none');
     })
     $('.inner').click(makeMenuSpin)
+    $('.item1').click(jobListMenuToggle)
+    $('.item4').click(jobListMenuToggle)
 }
 
 function makeMenuSpin(){
@@ -17,4 +13,14 @@ function makeMenuSpin(){
     $('.item3').toggleClass('spinItem3');
     $('.item4').toggleClass('spinItem4');
     $('.outer').toggleClass('hideBorder')
+}
+
+function jobListMenuToggle(){
+    $('.sidebar').toggleClass('sidebarHide')
+    $('.item1').toggleClass('select')
+}
+
+function jobListMenuToggle(){
+    $('.jobStats').toggleClass('jobStatsHide')
+    $('.item4').toggleClass('select')
 }
