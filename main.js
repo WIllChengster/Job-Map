8bea463d1edfa0298e41a83ce5b560f596757748
William Cheng <willchengsters@gmail.com>
2018-03-07 12:57:02 -0800

    spin animation on spinner menu

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ function initializeApp(){
     $('#jSearch').click(function(){
         $('.landing').css('display', 'none');
     })
+    $('.inner').click(function(){
+        $('.item1').toggleClass('spinItem1');
+        $('.item2').toggleClass('spinItem2');
+        $('.item3').toggleClass('spinItem3');
+        $('.item4').toggleClass('spinItem4');
+        $('.outer').toggleClass('hideBorder')
+    })
     getJobData();
 }
 
