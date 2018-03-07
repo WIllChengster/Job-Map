96d1fba3b8dfaa77de28c3ec1dd4873d7671d8dc
Matthew <matthewjkane@live.com>
2018-03-07 11:34:04 -0800

    Added initializeApp and other basic functionality

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(initializeApp);
+
+function initializeApp(){
+    $('#jSearch').click(newSearch);
+}
+
+function newSearch(){
+
+}
+
+class startSearch{
+    constructor(){
+        this.title = "Title";
+        this.location = "Location";
+    }
+}
