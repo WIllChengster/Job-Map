cb188ba37abddc355fa2905b6543f8ace7931ffa
William Cheng <willchengsters@gmail.com>
2018-03-07 21:16:58 -0800

    basic tooltip skeleton on the landing page

--- pageAnim.js
+++ pageAnim.js
@@ -1,7 +1,7 @@
 function makeMenuSpin(){
     $('.item1').toggleClass('spinItem1');
-    $('.item2').toggleClass('spinItem2');
-    $('.item3').toggleClass('spinItem3');
+    // $('.item2').toggleClass('spinItem2');
+    // $('.item3').toggleClass('spinItem3');
     $('.item4').toggleClass('spinItem4');
     $('.outer').toggleClass('hideBorder')
     $('svg').toggleClass('toggleDisplay');
