780180bf72d4e5acb2c9808c154ca9c20f377d86
unknown <briantmorr@gmail.com>
2018-04-27 13:24:02 -0700

    cleaned up faulty search paths in promise chain

--- javascript/main.js
+++ javascript/main.js
@@ -120,8 +120,7 @@ class startSearch {
             console.log('getJobdata resolved', resultData);
             this.jobData = resultData;
             if (findJobs.jobData.results.length === 0) {
-                $('.fadeOverlay, .noResultModal').toggleClass('toggleDisplay');
-
+                throw "no results for job search";
             } else {
                 return cleanAndPopulateMarkers();
             }
@@ -134,7 +133,8 @@ class startSearch {
             $('.spinner').toggleClass('toggleDisplay');
         })
             .catch(error => {
-            console.log("error in then chain, probably getJob", error);
+            console.log("error in then chain, error is: ", error);
+            $('.fadeOverlay, .noResultModal').toggleClass('toggleDisplay');
             $('#headerSearch').removeClass('noTouch');
             if (!($('.spinner').hasClass('toggleDisplay'))) {
                 $('.spinner').toggleClass('toggleDisplay');
